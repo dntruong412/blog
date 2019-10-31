e80a4afce0b585c42215fe61f6a899aeedefeef4
truong-dinh-vn <truongdn2@evolableasia.com>
2019-10-31 16:38:38 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="app">
+        <Importer v-for="(people, index) in fNames" :key="index" :id="index" :name="people" v-model="checkedNames" />
+        <br>
+        <span>Checked names: {{ checkedNames }}</span>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'app',
+        data() {
+            return {
+                fNames: ['jack', 'John', 'Mike', 'Schmit'],
+                checkedNames: []
+            }
+        },
+        components: {
+            Importer: () => import('./components/Importer.vue')
+        }
+    }
+</script>
---
+++ src/components/Importer.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <input type="checkbox" :id="id" :value="name" @change="change">
+        <label :for="id">{{name}}</label>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'Importer',
+        props: ['id', 'name'],
+        model: {
+            event: 'change'
+        },
+        methods: {
+            change($event) {
+                let currentValue = this.$attrs.value;
+                let elementValue = $event.target.value;
+
+                if ($event.target.checked) {
+                    currentValue.push(elementValue);
+                } else {
+                    currentValue.splice(currentValue.indexOf(elementValue), 1);
+                }
+
+                this.$emit('change', currentValue);
+            }
+        }
+    }
+</script>
